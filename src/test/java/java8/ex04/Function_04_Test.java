6e3ba7227b0f68e0f3929afbcdf12e907a430e6f
Camille <camille.hautois@gmail.com>
2019-02-13 18:13:40 +0100

    exercice 4

--- src/test/java/java8/ex04/Function_04_Test.java
+++ src/test/java/java8/ex04/Function_04_Test.java
@@ -33,7 +33,7 @@ public class Function_04_Test {
     // tag::adult[]
     // TODO Compléter la fonction
     // TODO AGE >=18
-    Predicate<Person> adult = null;
+    Predicate<Person> adult = age -> age.getAge() >= 18;
     // end::adult[]
 
     @Test
@@ -42,7 +42,7 @@ public class Function_04_Test {
         List<Person> personList = Data.buildPersonList();
 
         // TODO invoquer la méthode filter pour que le test soit passant
-        List<Person> result = null;
+        List<Person> result = filter(personList, adult);
 
         assertThat(result,  hasSize(4));
 
@@ -51,6 +51,7 @@ public class Function_04_Test {
     // PART 2 - ADULT AND LASTNAME=France AND FIRSTNAME=Armor
 
     // tag::predicateand[]
+    
     // TODO compléter la fonction
     // TODO le prédicat vérifie que le nom est "France"
     Predicate<Person> lastnameIsFrance = p -> p.getLastname().equals("France");
@@ -59,6 +60,7 @@ public class Function_04_Test {
     // TODO compléter la fonction
     // TODO le prédicat vérifie que le prénom est "Armor"
     Predicate<Person> firstnameIsArmor = p -> p.getFirstname().equals("Armor");
+    
     // end::predicateand[]
 
     @Test
@@ -68,7 +70,7 @@ public class Function_04_Test {
 
         // TODO invoquer la méthode filter pour que le test soit passant
         // TODO chaîner les prédicats adult, lastnameIsFrance et firstnameIsArmor avec la méthode and
-        List<Person> result = null;
+        List<Person> result = filter(personList, adult.and(lastnameIsFrance).and(firstnameIsArmor));
 
         assertThat(result,  hasSize(1));
         assertThat(result.get(0), hasProperty("firstname", is("Armor")));
